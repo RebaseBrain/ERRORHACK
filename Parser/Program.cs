f6b664193bb6d9fbbb006b3cddd1fb52e3e06f60
gohy <gohy2006@mail.ru>
2025-05-16 12:23:37 +0400

    feat(Program.cs): get log URLs

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+class Program
+{
+	static async Task Main()
+	{
+		string logUrl = "https://rdb.altlinux.org/api/export/beehive/ftbfs?branch=sisyphus";
+
+		HttpClient client = new HttpClient();
+		string logContent = await client.GetStringAsync(logUrl);
+		//logContent = "https://asdas asd httd a https://dsadad";
+		var urls = Regex.Matches(logContent, @"https?://[^\s""'>]+")
+				.Select(m => m.Value)
+				.ToList();
+
+		// foreach (string url in urls)
+		// {
+		// 	Console.WriteLine(url);
+		// }
+	}
+}
